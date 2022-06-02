a17d4065b668ae3684e8b5e433f5e867b0e45b45
Cup of Coding <anne@cupofcoding.com>
2022-06-02 00:59:04 -0700

    working on components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from 'react';  //Fragments let you group a list of children without adding extra nodes to the DOM.
 import './App.css';
 
+//components
+import InputTodo from './components/InputTodo';
+
 function App() {
-  return <Fragment></Fragment>;
+  return <Fragment>
+    <div className='container'>
+      <InputTodo />
+    </div>
+  </Fragment>;
 }
 
 export default App;
