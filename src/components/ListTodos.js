2df74cae3cfd625bff6bb2328729f55fe1fac867
Cup of Coding <anne@cupofcoding.com>
2022-06-04 01:06:03 -0700

    working on components; description data loads on page now :D

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -10,40 +10,56 @@ const ListTodos = () => {
     const getTodos = async() => {
         try {
 
-            const response = await fetch("http://localhost:5000/todos")
-            const jsonData = await response.json()
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
             //parse first to get json data after things are added
 
-            console.log(jsonData)
-            
+            //affected State triggers
+            setTodos(jsonData);
+
+            //console.log(jsonData)
         } catch (error) {
             console.error(error.message)
         }
-    }
+    };
 
+    //useEffect keeps making requests each time a render happens. 
     useEffect(() => {
         getTodos();
-    });
+    }, []);
+    // ', []' sets the restriction for 1 request. 
 
+    //current State
+    console.log(todos);
 
-    return <Fragment>
-        <table class="table mt-5 text-center">
-    <thead>
-      <tr>
-        <th>Description</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-        {/* --Structure Example
-        <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-        </tr>*/}      
-    </tbody>
-  </table></Fragment>;
+    return (<Fragment>
+        <table className="table mt-5 text-center">
+            <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {/* --Structure Example for mapping
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>john@example.com</td>
+                </tr>*/}
+                {todos.map(todo => (
+                <tr>
+                    <td>{todo.description}</td>
+                    {/* loads the results from todo.description */}
+                    <td>Edit</td>
+                    <td>Delete</td>
+                </tr>
+                ))}
+            </tbody>
+        </table>
+    </Fragment>
+    );
     // <h1>List Todos</h1>;
 };
 
