32bcbadd1936f679d3f802bf4a919abdcec60835
Cup of Coding <anne@cupofcoding.com>
2022-06-04 00:00:02 -0700

    working on components

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -1,6 +1,31 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+
+//useEffect will make a fetch request to a REST API every time component renders
 
 const ListTodos = () => {
+
+    //set useState
+    const [todos, setTodos] = useState([]);
+    
+    const getTodos = async() => {
+        try {
+
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+            //parse first to get json data after things are added
+
+            console.log(jsonData)
+            
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    });
+
+
     return <Fragment>
         <table class="table mt-5 text-center">
     <thead>
